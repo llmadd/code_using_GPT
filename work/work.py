3f7f952b4c5b74c52de4df6c11a946e0752fa9f0
qichen <38323944+llmadd@users.noreply.github.com>
2023-09-08 12:10:09 +0800

    Update work.py

--- work/work.py
+++ work/work.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import os
 from dotenv import load_dotenv
 from typing import Any, Dict, List, Optional, Sequence, Tuple, Union
